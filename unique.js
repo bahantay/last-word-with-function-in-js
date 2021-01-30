716971e808333ca53a8c660478f3df08e8273e61
bahantay <49163379+bahantay@users.noreply.github.com>
2021-01-31 00:19:19 +0600

    Create unique.js

---
+++ unique.js
@@ -0,0 +1,12 @@
+function my_array_uniq(arr) {
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+};
+my_array_uniq([])
